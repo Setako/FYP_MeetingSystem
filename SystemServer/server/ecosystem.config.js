859b3637db44a4c47dc860e59474f4acd2b3328d
Nocyx <originyx@gmail.com>
2019-02-12 14:53:23 +0800

    [Add] pm2 econsystem

---
+++ SystemServer/server/ecosystem.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    apps: [
+        {
+            name: 'commander',
+            script: 'dist/main.js',
+            node_args: [
+                '-r',
+                'ts-node/register/transpile-only',
+                '-r',
+                'tsconfig-paths/register',
+            ],
+            instances: process.env.WEB_CONCURRENCY || 1,
+            output: 'log/out.log',
+            error: 'log/err.log',
+            merge_logs: true
+        },
+    ],
+};
