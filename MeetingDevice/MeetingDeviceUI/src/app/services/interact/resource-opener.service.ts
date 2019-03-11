35d9c01d7dce48885037e1993772e27d20def54f
Setako <zygdtako@outlook.com>
2019-03-11 15:24:44 +0800

    Added more resource player

--- MeetingDevice/MeetingDeviceUI/src/app/services/interact/resource-opener.service.ts
+++ MeetingDevice/MeetingDeviceUI/src/app/services/interact/resource-opener.service.ts
@@ -3,6 +3,9 @@ import {WindowStackService} from '../window/window-stack.service';
 import {SlideShowPlayerComponent} from '../../shared/components/resource-player/slide-show-player/slide-show-player.component';
 import {DocumentPlayerComponent} from '../../shared/components/resource-player/document-player/document-player.component';
 import {ControllableComponent} from '../../shared/components/controllable/controllable.component';
+import {AudioPlayerComponent} from '../../shared/components/resource-player/audio-player/audio-player.component';
+import {ImagePlayerComponent} from '../../shared/components/resource-player/image-player/image-player.component';
+import {VideoPlayerComponent} from '../../shared/components/resource-player/video-player/video-player.component';
 
 @Injectable({
     providedIn: 'root'
@@ -11,6 +14,11 @@ export class ResourceOpenerService {
     private typeMap: { [type: string]: Type<ControllableComponent> } = {
         'application/vnd.google-apps.presentation': SlideShowPlayerComponent,
         'application/vnd.google-apps.document': DocumentPlayerComponent,
+        'application/vnd.google-apps.audio': AudioPlayerComponent,
+        'application/vnd.google-apps.photo': ImagePlayerComponent,
+        'application/vnd.google-apps.drawing': ImagePlayerComponent,
+        // 'application/vnd.google-apps.spreadsheet',
+        'application/vnd.google-apps.video': VideoPlayerComponent,
     };
 
     constructor(private windowStack: WindowStackService) {
--- MeetingDevice/MeetingDeviceUI/src/app/shared/components/resource-player/image-player/image-player.component.ts
+++ MeetingDevice/MeetingDeviceUI/src/app/shared/components/resource-player/image-player/image-player.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, OnInit, ViewChild} from '@angular/core';
+import {ControllableComponent} from '../../controllable/controllable.component';
+import {WindowData} from '../../../../services/window/window-data';
+import {RobotService} from '../../../../services/robot.service';
+import {WINDOW_DATA} from '../../../../services/window/window-ref';
+import {interval} from 'rxjs';
 
 @Component({
-  selector: 'app-image-player',
-  templateUrl: './image-player.component.html',
-  styleUrls: ['./image-player.component.css']
+    selector: 'app-image-player',
+    templateUrl: './image-player.component.html',
+    styleUrls: ['./image-player.component.css']
 })
-export class ImagePlayerComponent implements OnInit {
+export class ImagePlayerComponent extends ControllableComponent
+    implements OnInit, AfterViewInit {
+    @ViewChild('webContent')
+    webContent: ElementRef;
+    url: string;
 
-  constructor() { }
+    constructor(
+        @Inject(WINDOW_DATA) data: WindowData<ImagePlayerComponent>,
+        private robot: RobotService
+    ) {
+        super();
+        this.url = data.data;
+        // this.url = 'http://taiko.pui.pm';
+    }
 
-  ngOnInit() {
-  }
+    remoteControl(data: any) {
+        console.log(data.type);
+        switch (data.type) {
+            case 'down':
+                this.tap(data.position);
+                break;
+        }
+    }
 
+    tap(position: number[]) {
+        console.log(position);
+    }
+
+    ngOnInit() {
+    }
+
+    ngAfterViewInit(): void {
+        interval(500).subscribe(() => this.webContent.nativeElement.focus());
+    }
 }
