927b5d351ad1459e46a1f501c3d075618e37ddd7
Setako <zygdtako@outlook.com>
2019-04-23 23:36:53 +0800

    [Add] calendar time limit

--- WebAppClient/src/app/pages/member/meeting/meeting-edit/meeting-edit-time/meeting-edit-time.component.ts
+++ WebAppClient/src/app/pages/member/meeting/meeting-edit/meeting-edit-time/meeting-edit-time.component.ts
@@ -17,14 +17,34 @@ export class MeetingEditTimeComponent implements OnInit {
     new Date('2019-01-16 14:30'),
     new Date('2019-01-17 16:30'),
   ];
+  searchTimeStart = '00:00';
+  searchTimeEnd = '24:00';
 
   constructor() {
   }
 
+  get searchTimeStartFormated() {
+    console.log(this.formatHour(this.searchTimeStart));
+    return this.formatHour(this.searchTimeStart);
+  }
+
+  get searchTimeEndFormated() {
+    return this.formatHour(this.searchTimeEnd);
+  }
+
   get timeAutoComplete() {
+    console.log(this.searchTimeStart);
     return Array.from(new Array(48), (x, i) => i)
       .map((x) => ({hour: Math.floor(x / 2), minute: (x % 2) * 30}))
       .map((t) => `${(t.hour + '').padStart(2, '0')}:${(t.minute + '').padStart(2, '0')}`);
+  }
+
+  private formatHour(str: string) {
+    const hourStr = str.split(':')[0];
+    const minStr = str.split(':')[1];
+    const hour = Math.max(0, Math.min(24, parseInt(hourStr, 10) || 0));
+    const min = parseInt(minStr, 10) || 0;
+    return hour + (min === 0 ? 0 : 0.5);
   }
 
   ngOnInit() {
